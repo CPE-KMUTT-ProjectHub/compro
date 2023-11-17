4a03f85b5581559b9cf06a18bd8315dfb0cd2ce8
JoyChanya <chanya.joy23@gmail.com>
2023-11-18 01:07:15 +0700

    login system 😏
    first commit

--- code.c
+++ code.c
@@ -70,7 +70,48 @@ void show(){
         printf("\n");
     }
 }
+
+// This is a login system function (admin or cashier) // Chanya Kittichai 66070503412
+
+int login()
+{
+    char code;
+    printf("[0] Admin\n[1] Cashier\n[else] Exit\n");
+    scanf("%c", &code);
+
+    char psw[6];
+
+    if ( code == '0')
+    {
+        printf("Please enter a password: ");
+        scanf("%s", psw);
+        while (strcmp(psw, "12345a")!=0)
+        {
+            printf("Please enter the password again.\n");
+            scanf("%s", psw);
+        }
+        printf("Yeah! You are admin.\n");
+        
+    }
+    if ( code == '1')
+    {
+        printf("Please enter a password: ");
+        scanf("%s", psw);
+        while (strcmp(psw, "12345c")!=0)
+        {
+            printf("Please enter the password again.\n");
+            scanf("%s", psw);
+        }
+        printf("Yeah! You are cashier.\n");
+    }
+    
+    return 0;
+    
+}
+
+
 int main() {
-    show();
+    login();
+    //show();
     return 0;
 }
