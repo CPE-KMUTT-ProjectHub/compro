63dcadb2395912a99b4af5a537f75c3d92a4de94
Earth <63118195+EarthWittawat@users.noreply.github.com>
2023-11-18 00:10:15 +0700

    first commit with read csv function

---
+++ code.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_SIZE 256
+#define MAX_NAME_SIZE 100
+
+struct product {
+    char Name[MAX_NAME_SIZE];
+    char Brand[MAX_NAME_SIZE];
+    char ProducType[MAX_NAME_SIZE];
+    int AmountInStorage;
+    float Price;
+    char ExpireDate[MAX_LINE_SIZE];
+};
+
+void readCSV(const char *filename, struct product *_product, int *numProduct) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[MAX_LINE_SIZE];
+    *numProduct = 0;
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        char *token = strtok(line, ",");
+        
+        strncpy(_product[*numProduct].Name, token, MAX_NAME_SIZE - 1);
+        _product[*numProduct].Name[MAX_NAME_SIZE - 1] = '\0';
+
+        token = strtok(NULL, ",");
+        strncpy(_product[*numProduct].Brand, token, MAX_NAME_SIZE - 1);
+        _product[*numProduct].Brand[MAX_NAME_SIZE - 1] = '\0';
+
+        token = strtok(NULL, ",");
+        strncpy(_product[*numProduct].ProducType, token, MAX_NAME_SIZE - 1);
+        _product[*numProduct].ProducType[MAX_NAME_SIZE - 1] = '\0';
+
+        token = strtok(NULL, ",");
+        sscanf(token, "%d", &_product[*numProduct].AmountInStorage);
+
+        token = strtok(NULL, ",");
+        sscanf(token, "%f", &_product[*numProduct].Price);
+
+        token = strtok(NULL, ",");
+        strncpy(_product[*numProduct].ExpireDate, token, MAX_NAME_SIZE - 1);
+        _product[*numProduct].ExpireDate[MAX_NAME_SIZE - 1] = '\0';
+
+        (*numProduct)++;
+    }
+
+    fclose(file);
+}
+void show(){
+    const char *filename = "priceyCosmetics.csv";
+    struct product _product[1000];
+    int numSubject;
+    FILE *fpt;
+    readCSV(filename, _product, &numSubject);
+
+    for (int i = 0; i < numSubject; i++) {
+        printf("%s ", _product[i].Name);
+        printf("%s ", _product[i].Brand);
+        printf("%s ", _product[i].ProducType);
+        printf("%d ", _product[i].AmountInStorage);
+        printf("%.2f ", _product[i].Price);
+        printf("%s", _product[i].ExpireDate);
+        printf("\n");
+    }
+}
+int main() {
+    show();
+    return 0;
+}
